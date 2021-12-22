4776161b9d09c61957e1fd6b8b8dfbfd139eedd3
Sabrina459 <sabrinas.ua82@gmail.com>
2021-12-22 22:55:43 +0200

    adding back to database connection

---
+++ web/backend/repositories/user_repository.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql2/promise')
+const { db } = require('../config')
+
+class UserRepository {
+
+    async create(user) {
+        try {
+            const { username, password, firstName, lastName } = user
+            const sql = 'INSERT INTO user VALUES (NULL, ?, ?, ?, ?)'
+            const conn = await mysql.createConnection(db)
+            const [rows] = await conn.query(sql, [username, password, firstName, lastName])
+            await conn.end()
+            return rows.insertId
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+
+    async findByUsername(username) {
+        try {
+            const sql = 'SELECT * FROM user WHERE username = ?'
+            const conn = await mysql.createConnection(db)
+            const [rows] = await conn.query(sql, [username])
+            await conn.end()
+            return rows[0]
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+}
+
+module.exports = UserRepository
